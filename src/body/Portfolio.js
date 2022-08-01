33ee8f946dac6041207809367e2507e5862b76eb
JMJHOX <josemiguelaparicio507@gmail.com>
2022-08-01 11:13:52 -0500

    fix

--- src/body/Portfolio.js
+++ src/body/Portfolio.js
@@ -1,7 +1,44 @@
+import { useQuery, gql } from "@apollo/client";
+import { MultiActionAreaCard } from "./../components/Card";
 import "./Portfolio.css";
 
+const REPO_DATA = gql`
+  query GithubUserRequest {
+    user(login: "JMJHOX") {
+      pinnedItems(first: 6, types: REPOSITORY) {
+        nodes {
+          ... on Repository {
+            name
+            description
+            url
+            resourcePath
+            stargazerCount
+          }
+        }
+      }
+    }
+  }
+`;
 
-export async function Portfolio() {
- const dataApi =   await  fetch("https://api.jikan.moe/v4/anime").then(response => response.json()).then(json => json.data)
- console.log(dataApi)
+export function Portfolio() {
+  const { data } = useQuery(REPO_DATA);
+  const userRepoGraph = data?.user.pinnedItems.nodes;
+  console.log(userRepoGraph);
+  return (
+    <div id="portfolio">
+      <p>Top proyectos realizados </p>
+      <div className="container-custom">
+        {userRepoGraph?.map((e) => {
+          return (
+            <MultiActionAreaCard
+              name={e.name}
+              desc={e.description}
+              stars={e.stargazerCount}
+              urlPath={e.url}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
